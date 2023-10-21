e76f7a9a7df5c64f9b6269402dbb4b520a16b796
HASBIALLAH OYEBO <143263920+hasbiyallah01@users.noreply.github.com>
2023-10-21 10:02:55 +0100

    Update Program.cs

--- Chapter4Input/Program.cs
+++ Chapter4Input/Program.cs
@@ -257,16 +257,16 @@
 // printRealNum(35);
 
 //QUESTION 11
-void fibonacci()
-{
-    Console.Write("Enter the num you want to sumup to:");
-    int num=int.Parse(Console.ReadLine());
-    int total=0;
-    for(int i=1;i<num;i++)
-    {
-       total+=i;
-    }
-}
+// void fibonacci()
+// {
+//     Console.Write("Enter the num you want to sumup to:");
+//     int num=int.Parse(Console.ReadLine());
+//     int total=0;
+//     for(int i=1;i<num;i++)
+//     {
+//        total+=i;
+//     }
+// }
 
 
 
